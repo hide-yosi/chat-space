46a3395d9222a694113653e9c444740b49f52bb2
hide-nori <hidenori457@icloud.com>
2020-01-04 18:42:28 +0900

    自動更新の修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -53,22 +53,7 @@ $(function(){
   };
   $('.new_message').on('submit', function(e) {
     e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-
-    $.ajax({
-      url: url,  //同期通信でいう『パス』
-      type: 'POST',  //同期通信でいう『HTTPメソッド』
-      data: formData,  
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-
-    .done(function(data){
-      var html = buildHTML(data);
-    $('.migisita').append(html);
-    $('.text').val('');
+    
     $('.form__submit').prop('disabled', false);
     $('.migisita').animate({ scrollTop: $('.migisita')[0].scrollHeight});
     $("form")[0].reset();
@@ -91,26 +76,6 @@ $(function(){
       dataType: 'json',
     })
     .done(function(data){ 
-      console.log("___data___");
-      console.log(data);
-     if (data.length !==0) {
-      var insertHTML = '';
-      $.each(data, function(i, message) {
-        insertHTML += buildHTML(data)
-      });
-      $('.migisita').append(insertHTML);
-      //var html = buildHTML(data);
-      //$('.migisita').append(html);
-       $('.form__submit').prop('disabled', false);
-       $('.migisita').animate({ scrollTop: $('.migisita')[0].scrollHeight});
-       $(".form__mask").prop('disabled', false);
-        return false;
-     }
-    })
-   .fail(function(){
-     console.log('FAIL');
-     alert("メッセージ送信に失敗しました");
-   });
   }
   if (document.location.href.match(/\/groups\/\d+\/messages/)){
    setInterval(reloadMessages, 7000);
